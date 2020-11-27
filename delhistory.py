0d5541ef2390c128362ecd8647573fd78103a517
aa-ag <aaron.aguerrevere@gmail.com>
2020-11-27 17:03:39 -0600

    new version

--- delhistory.py
+++ delhistory.py
@@ -1,45 +1,21 @@
 ###--- DELETE BROWSER HISTORY ---###
-import os
 from selenium import webdriver
-import sqlite3
+from selenium.webdriver.support.ui import WebDriverWait
 
 #--- Delete from database with Python ---#
 
-def clear_cache(browser):
-    '''
-    Clear cookies and cache from ChromeDriver instance.
-    '''
-    try:
-        browser.get('chrome//settings/clearBrowserData')
-        browser.find_element_by_xpath('//settings-ui').send_keys(Keys.ENTER)
+def get_button(driver):
+    return driver.find_element_by_css_selector('* /deep/ #clearBrowsingDataConfirm')
 
-    except Exception as x:
-        print(str(x))
-        browser.quit()
+def clear_cache(driver, timeout=60):
+    driver.get('chrome://settings/clearBrowserData')
 
-browser = webdriver.Chrome('/usr/local/bin/chromedriver')
-clear_cache(browser)
+    wait = WebDriverWait(driver, timeout)
+    wait.until(get_button)
 
-#--- Delete from database in machine with Python ---#
-# conn = sqlite3.connect("/Users/aaronaguerrevere/Library/Safari/History.db")
+    get_button(driver).click()
 
-# cursor = conn.cursor()
+    wait.until_not(get_button)
 
-# id = 0
-
-# result = True
-
-# while result:
-#     ids = list()
-
-#     for row in cursor.execute("SELECT * FROM urls"):
-#         print(row)
-
-#         id = row[0]
-#         ids.append((id,))
-
-#     cursor.executemany('DELETE * FROM urls WHERE id = ?', ids)
-
-#     conn.commit()
-
-# conn.close()
+driver = webdriver.Chrome()
+clear_cache(driver)
